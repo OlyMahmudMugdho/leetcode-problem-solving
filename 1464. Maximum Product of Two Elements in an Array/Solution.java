69e8bd973370a06bfd4bfb49286e32d0a8b75a78
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-12-18 20:07:15 +0600

    optional problem week-1 AT-UCSD

---
+++ 1464. Maximum Product of Two Elements in an Array/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int length = nums.length;
+        int maxProduct = 0;
+        for(int i=0;i<length;i++){
+            for(int j = 0; j<length;j++){
+                if(i==j){
+                    continue;
+                }
+                else {
+                    if(((nums[i]-1)*(nums[j]-1)) > maxProduct){
+                        maxProduct = (nums[i]-1)*(nums[j]-1);
+                        continue;
+                    }
+                }
+            }
+        }
+        return maxProduct;
+    }
+}
