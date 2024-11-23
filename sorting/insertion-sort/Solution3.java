e237d493c0a9a94216e92be3dd8d41bdfaecab90
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-11-23 20:05:31 +0600

    insertion sort 3

---
+++ sorting/insertion-sort/Solution3.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Solution3 {
+
+    public static void sort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 4, 2, 8, 3, 1 };
+        System.out.println(Arrays.toString(nums));
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
