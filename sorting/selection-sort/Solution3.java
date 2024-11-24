53e6db100d6e3a3c600aeb8392eacb99c41b75ca
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-11-24 11:53:39 +0600

    selection sort 3

---
+++ sorting/selection-sort/Solution3.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Solution3 {
+
+    public static void sort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int min = nums[i];
+            int minIndex = i;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < min) {
+                    min = nums[j];
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = nums[i];
+                nums[i] = nums[minIndex];
+                nums[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 13, 46, 24, 52, 20, 9 };
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
