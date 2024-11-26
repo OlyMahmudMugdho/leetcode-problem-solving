275d2c890ecbee35ebc657cb151826f2082cd676
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-11-26 14:15:57 +0600

    selection sort 4

---
+++ sorting/selection-sort/Solution4.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Solution4 {
+
+    public static void sort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int minIndex = i;
+            int min = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < min) {
+                    min = nums[j];
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = nums[i];
+                nums[i] = nums[minIndex];
+                nums[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 13, 46, 24, 52, 20, 9 };
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
